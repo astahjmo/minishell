e6da335f4b23ff734e84be5a4eaa521bec0f619a
johmatos <contato.johnatan06@gmail.com>
2023-02-28 23:28:30 +0100

    Backup

--- src/prompt/wait_input.c
+++ src/prompt/wait_input.c
@@ -6,25 +6,24 @@
 /*   By: johmatos <johmatos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 14:17:45 by johmatos          #+#    #+#             */
-/*   Updated: 2023/02/08 19:40:08 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/02/23 13:24:38 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-#include <readline/readline.h>
 #include <stdlib.h>
 
 int	wait_input(int argc, char *argv[], char *envp[])
 {
 	char	*prompt;
-	char	*cmd;
+	char	*line;
 
-	while (1)
+	while (1) 
 	{
 		prompt = get_prompt();
-		cmd = readline(prompt);
+		line = readline(prompt);
+		line_analysis(line);
 		free(prompt);
-		free(cmd);
 	}
 	return (1);
 }
