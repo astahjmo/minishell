063d03d34102fcb641a59df184f932a9f56caa82
Johnatan Matos de oliveira <johmatos@c3r13p4.42sp.org.br>
2023-05-23 17:54:04 -0300

    add: handler to doubled and single quotes

--- src/tokenizer/handler_expansions.c
+++ src/tokenizer/handler_expansions.c
@@ -6,8 +6,29 @@
 /*   By: johmatos <johmatos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 05:32:09 by johmatos          #+#    #+#             */
-/*   Updated: 2023/05/20 05:46:42 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/05/23 17:53:14 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static char     j
+#include "minishell.h"
+
+char	*expand_dolar(char *line)
+{
+	char	*start;
+	char	*final;
+	char	*key;
+	char	*str;
+
+	start = ft_strchr(line, '"');
+	final = start;
+	if (start == NULL)
+		return (NULL);
+	start++;
+	while(!string_is_equal(line, ' ') && get_token(line) == -1
+		&& get_expansion(line) == -1)
+		final++;
+	key = ft_substr(start, 0, final - start);
+	str = get_env(key);
+	free(key);
+	return (str);
+}
