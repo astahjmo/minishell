c3b2e39432b2bb7f65024426c43b9b46b9598a6e
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-06-10 17:16:16 -0300

    Started by fixing the size of the buffers
    ENV_CONTENT_SZ should be 32367 and ENV_NAME_SZ 1024
    Also, the HEAP_OVERFLOW_PROTECTION macro was named incorrectly as
    BUFF_OVERFLOW_PROTECTION
    
    	modified:   include/minishell.h
    	modified:   src/builtins/env_cmd.c
    	modified:   src/env/init_env.c
    	new file:   src/utils/get_env.c

--- src/utils/get_env.c
+++ src/utils/get_env.c
@@ -1,25 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env_cmd.c                                          :+:      :+:    :+:   */
+/*   get_env.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/09 16:26:54 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/06/09 16:26:54 by vcedraz-         ###   ########.fr       */
+/*   Created: 2023/06/10 17:08:28 by vcedraz-          #+#    #+#             */
+/*   Updated: 2023/06/10 17:14:12 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	env_cmd(char *env[ENV_CONTENT_SZ], int number_of_envs)
-{
-	int	i;
-
-	i = -1;
-	while (++i < number_of_envs)
-	{
-		ft_putstr_fd(env[i], 1);
-		ft_putchar_fd('\n', 1);
-	}
-}
+void get_env(char name[1024], char value[)
