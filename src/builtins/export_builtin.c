552e1c7146d157651374df83cce5b39e6371312a
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-06-10 21:10:30 -0300

    second step was to fix export first
    -> I realized export implementation was fairly incomplete so I started
    fixing export first (now it used the sytax analysis before doing
    anything and then it checks for the specific rules for a variable NAME
    -> I implemented a preliminary version of unset as well
    (todo): finish fixing export by making it behave like bash when we enter
    it without any arguments
    
    	modified:   include/minishell.h
    	modified:   src/builtins/export_builtin.c
    	modified:   src/builtins/unset_builtin.c
    	modified:   src/executor/executor.c
    	modified:   src/prompt/wait_input.c
    	modified:   src/syntax/analysis.c

--- src/builtins/export_builtin.c
+++ src/builtins/export_builtin.c
@@ -12,8 +12,34 @@
 
 #include "minishell.h"
 
+static int	is_valid_env_name(char *name_value);
+
 void	export_builtin(t_databus *data, char *new_env)
 {
-	if (++data->number_of_envs < HEAP_OVERFLOW_PROTECTION)
-		data->env[data->number_of_envs - 1] = ft_strdup(new_env);
+	if (!is_valid_env_name(new_env))
+	{
+		printf("minishell: export: `%s': not a valid identifier\n", new_env);
+		return ;
+	}
+	if (!ft_strchr(new_env, '='))
+		return ;
+	if (is_valid_syntax(data->cmds->head))
+	{
+		if (++data->number_of_envs < HEAP_OVERFLOW_PROTECTION)
+			data->env[data->number_of_envs - 1] = ft_strdup(new_env);
+	}
+}
+
+static int	is_valid_env_name(char *name_value)
+{
+	if (!ft_isalpha(*name_value) && *name_value != '_')
+		return (0);
+	name_value++;
+	while (*name_value != '\0' && *name_value != '=')
+	{
+		if (!ft_isalnum(*name_value) && *name_value != '_')
+			return (0);
+		name_value++;
+	}
+	return (1);
 }
--- src/builtins/unset_builtin.c
+++ src/builtins/unset_builtin.c
@@ -11,3 +11,17 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	unset_builtin(t_databus *data, char *env_to_unset)
+{
+	int	len;
+
+	len = ft_strlen(env_to_unset);
+	if (!len)
+		return ;
+	if (!ft_strncmp(data->env[data->number_of_envs - 1], env_to_unset, len + 1))
+		return ;
+	free(data->env[data->number_of_envs - 1]);
+	data->env[data->number_of_envs - 1] = NULL;
+	data->number_of_envs--;
+}
