9509164c440debac3f1188156926cd688ca4eaf4
Vinni-Cedraz <Vinni-Cedraz@gmail.com>
2023-06-10 22:09:10 -0300

    export behaves like bash with empty arguments
    also, "declare -x" calls export
    
    	modified:   include/minishell.h
    	modified:   src/builtins/env_builtin.c
    	modified:   src/executor/executor.c

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -20,6 +20,10 @@ void	executor(t_databus *data)
 		env_builtin(data->env, data->number_of_envs);
 	else if (!ft_strncmp(data->stream, "export ", 7))
 		export_builtin(data, &data->stream[7]);
+	else if (!ft_strncmp(data->stream, "export", 7))
+		alt_env_builtin(data->env, data->number_of_envs);
+	else if (!ft_strncmp(data->stream, "declare -x ", 11))
+		export_builtin(data, &data->stream[11]);
 	else if (!ft_strncmp(data->stream, "unset ", 6))
 		unset_builtin(data, &data->stream[6]);
 	return ;
