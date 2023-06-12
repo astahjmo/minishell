75d620974a62d36cb41f3b41f7fb22809b0c0715
astahjmo <contato.johnatan06@gmail.com>
2023-06-12 15:16:13 -0300

    Fix: analysis always returning true

--- src/syntax/analysis.c
+++ src/syntax/analysis.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   analysis.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: astaroth <astaroth@student.42.fr>          +#+  +:+       +#+        */
+/*   By: johmatos <johmatos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 16:10:41 by astaroth          #+#    #+#             */
-/*   Updated: 2023/05/18 16:14:31 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/06/12 15:15:23 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,12 +69,8 @@ int	is_valid_syntax(t_node *head)
 		progress = get_next_state(state, cursor->next);
 		cursor = cursor->next;
 	}
-	if (progress == -1)
-	{
-		ft_printf("Error: %s\n", get_token_string(state));
+	if (progress != -1)
 		return (0);
-	}
-	else
-		return (1);
-	return (0);
+	ft_printf("Error: %s\n", get_token_string(state));
+	return (1);
 }
