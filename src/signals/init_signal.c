f2a698f00785d587e14fa4fd5dc407923a830704
astahjmo <contato.johanatn06@gmail.com>
2023-06-12 17:59:34 -0300

    Fix: sa_mask to sigint

--- src/signals/init_signal.c
+++ src/signals/init_signal.c
@@ -6,7 +6,7 @@
 /*   By: johmatos <johmatos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 15:59:11 by johmatos          #+#    #+#             */
-/*   Updated: 2023/06/12 17:51:30 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/06/12 17:58:48 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,11 @@ void	init_signal(void)
 	sa_sigquit.sa_handler = SIG_IGN;
 	sa_sigquit.sa_flags = SA_RESTART;
 	sigemptyset(&sa_sigquit.sa_mask);
+	sigemptyset(&sa_sigint.sa_mask);
 	sa_sigint.sa_handler = define_handle;
 	sa_sigint.sa_flags = SA_RESTART;
 	sigaddset(&sa_sigint.sa_mask, SIGINT);
+	sigaddset(&sa_sigquit.sa_mask, SIGQUIT);
 	sigaction(SIGINT, &sa_sigint, NULL);
 	sigaction(SIGQUIT, &sa_sigquit, NULL);
 }
