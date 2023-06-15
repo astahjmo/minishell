b7848ce53ca23f2cde64dd8421ee6971bc42e027
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-06-15 13:08:11 -0300

    make export behave more like in bash
    export without arguments should display all initialized variables (even
    those initialized with an empty string) with double quotes around it's
    content
        --> and this means uninitialized ones should have neither content
        nor any quotes around anything
    
    	modified:   src/builtins/env_builtin.c

--- src/builtins/env_builtin.c
+++ src/builtins/env_builtin.c
@@ -12,7 +12,7 @@
 
 #include "minishell.h"
 
-void	env_builtin(t_databus *d)
+void	env_builtin(t_databus *data)
 {
 	int		i;
 	char	name[STR_LIMIT];
@@ -21,30 +21,29 @@ void	env_builtin(t_databus *d)
 	i = -1;
 	ft_bzero(name, STR_LIMIT);
 	ft_bzero(content, STR_LIMIT);
-	while (++i < d->number_of_envs)
+	while (++i < data->number_of_envs)
 	{
-		get_env_name(d, name, d->env[i]);
-		get_env_content(d, content, name);
+		get_env_name(data, name, data->env[i]);
+		get_env_content(data, content, name);
 		if (*content)
-		{
-			ft_putstr_fd(d->env[i], 1);
-			ft_putchar_fd('\n', 1);
-		}
+			printf("%s\n", data->env[i]);
 	}
 }
 
 void	alt_env_builtin(t_databus *data)
 {
-	int	i;
+	int		i;
+	char	name[STR_LIMIT];
+	char	content[STR_LIMIT];
 
 	i = -1;
 	while (++i < data->number_of_envs)
 	{
-		if (data->env[i])
-		{
-			ft_putstr_fd("declare -x ", 1);
-			ft_putstr_fd(data->env[i], 1);
-			ft_putchar_fd('\n', 1);
-		}
+		get_env_name(data, name, data->env[i]);
+		get_env_content(data, content, name);
+		if (ft_strchr(data->env[i], '='))
+			printf("declare -x %s=\"%s\"\n", name, content);
+		else
+			printf("declare -x %s\n", name);
 	}
 }
