ca707afa0af6da701f6e0a3735e3d8ae3a1d07c0
Vinni-Cedraz <Vinni.vcr@gmail.com>
2023-06-17 14:00:57 -0300

    FIXING MANY THINGS:
    Every lost commit from the things I did 06/15 is corrected and included
    at once in this commit (except for unset with multiple arguments).
    
    	modified:   include/minishell.h
    	modified:   src/builtins/env_builtin.c
    	modified:   src/builtins/utils_builtins.c

--- src/builtins/utils_builtins.c
+++ src/builtins/utils_builtins.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   utils_builtins.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 10:06:28 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/06/15 10:09:55 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/06/17 13:57:29 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-inline int	is_initialized_to_zero(char *str)
+int	is_initialized_to_zero(char *str)
 {
 	int	i;
 
@@ -65,29 +65,4 @@ int	cmptok(char *s1, char *s2, char tok)
 			break ;
 	}
 	return (TRUE);
-
-void	get_env_name(char *name, char *env)
-{
-	char	*equal_sign;
-
-	equal_sign = ft_strchr(env, '=');
-	ft_strlcpy(name, env, equal_sign - env + 1);
-	return ;
-}
-
-void	get_env_content(char *content, char *name, char *env)
-{
-	char	*found_name;
-	char	*equal_sign;
-
-	found_name = ft_strnstr(env, name, STR_LIMIT);
-	if (found_name)
-	{
-		equal_sign = ft_strchr(found_name, '=');
-		if (equal_sign && equal_sign[1])
-			ft_strlcpy(content, &equal_sign[1], ft_strlen(equal_sign));
-		else if (equal_sign && !(equal_sign[1]))
-			ft_bzero(content, STR_LIMIT);
-		return ;
-	}
 }
