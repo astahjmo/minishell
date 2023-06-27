5767965534ede05b750db2eeab9bdd53344c5d28
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-06-27 17:24:21 -0300

    fixed segfault when env string is too long

--- src/builtins/init_env.c
+++ src/builtins/init_env.c
@@ -15,20 +15,24 @@
 void	init_env(t_databus *data)
 {
 	int		i;
+	int		j;
 	int		envlen;
 	char	**envp;
 
-	i = -1;
+	i = 0;
+	j = 0;
 	envp = data->envp;
 	data->number_of_envs = 0;
 	while (envp[data->number_of_envs] != NULL)
 		data->number_of_envs++;
 	if (data->number_of_envs >= ENVS_LIMIT)
 		data->number_of_envs = ENVS_LIMIT / 2;
-	while (++i < data->number_of_envs)
+	while (i < data->number_of_envs)
 	{
-		envlen = ft_strlen(envp[i]) + 1;
+		envlen = ft_strlen(envp[j]) + 1;
 		if (envlen <= STR_LIMIT)
-			ft_strlcpy(data->env[i], envp[i], envlen);
+			ft_strlcpy(data->env[i++], envp[j++], envlen);
+		else
+			j++;
 	}
 }
