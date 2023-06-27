6accf73b5d46eacba236def342476cfeba741444
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-06-27 16:54:16 -0300

    back to the segfault
    on purpose!
    
    modified:   include/minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -38,7 +38,7 @@
 # define RED "\033[0;31m"
 # define RESET "\033[0m"
 # define STR_LIMIT 1024
-# define ENVS_LIMIT 8512
+# define ENVS_LIMIT 1024
 # define SIZEMAX 9223372036854775807
 
 typedef struct s_node		t_node;
