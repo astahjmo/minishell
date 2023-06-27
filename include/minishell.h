2ed6787e9b24f9dae39f6543e631fa64e00c66b8
Vinni-Cedraz <Vinni.vcr@gmail.com>
2023-06-27 16:40:58 -0300

    restoring erroneous change on header
    modified:   include/minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -37,8 +37,8 @@
 # define GREEN "\033[0;32m"
 # define RED "\033[0;31m"
 # define RESET "\033[0m"
-# define STR_LIMIT 512
-# define ENVS_LIMIT 8512
+# define STR_LIMIT 1024
+# define ENVS_LIMIT 1024
 # define SIZEMAX 9223372036854775807
 
 typedef struct s_node		t_node;
