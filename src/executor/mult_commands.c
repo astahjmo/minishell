12dca7602f0304ec4b8592b61313f339196bf05d
astahjmo <contato.johanatn06@gmail.com>
2023-07-16 15:17:24 -0300

    feat: mult commands

--- src/executor/mult_commands.c
+++ src/executor/mult_commands.c
@@ -6,7 +6,7 @@
 /*   By: johmatos <johmatos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 20:14:51 by johmatos          #+#    #+#             */
-/*   Updated: 2023/07/07 21:18:15 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/07/16 15:02:53 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,53 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int	child_routine(t_node *cmds);
+static void	dup2_and_close(int fd, int clone)
+{
+	dup2(fd, clone);
+	close(fd);
+}
+
+void	child_routine(t_node *cmds, int count)
+{
+	t_process_io	*fds;
+
+	fds = command_hook(count);
+	if (fds->input > 2)
+		dup2_and_close(fds->input, STDIN_FILENO);
+	if (fds->output > 2)
+		dup2_and_close(fds->output, STDOUT_FILENO);
+	exec_command(cmds);
+	exit(1);
+}
 
 int	main_routine(t_node *cmds);
 
-int	fork_and_execute(t_node *cmds)
+void	mult_command(t_node **cmds)
 {
+	int		pipe_fds[2];
+	int		bkp_fd;
+	int		cmd_count;
 	pid_t	pid;
 
-	pid = fork();
-	if (pid < 0)
-		ft_printf("deu merda");
-	if (pid == 0)
-		child_routine(cmds);
-	else
-		main_routine(cmds);
-	return (pid);
-}
-
-void	mult_command(t_node *cmds)
-{
-	t_tokens		builtin_idx;
-	t_fn_built_exec	**exec;
-
-	exec = get_built_func();
-	builtin_idx = is_builtin(cmds->str);
-	if (builtin_idx != -1)
-		exec[builtin_idx](cmds);
-	else
-		exec_command(cmds);
+	cmd_count = 0;
+	bkp_fd = STDIN_FILENO;
+	while (cmds[cmd_count])
+	{
+		pipe(pipe_fds);
+		pid = fork();
+		if (pid == 0)
+		{
+			if (bkp_fd != 0)
+				dup2(bkp_fd, STDIN_FILENO);
+			close(pipe_fds[0]);
+			if (cmd_count != getter_data()->cmds->idx)
+				dup2(pipe_fds[1], STDOUT_FILENO);
+			child_routine(cmds[cmd_count], cmd_count);
+		}
+		cmd_count++;
+		bkp_fd = dup(pipe_fds[0]);
+		close(pipe_fds[0]);
+		close(pipe_fds[1]);
+	}
+	close(bkp_fd);
 }
