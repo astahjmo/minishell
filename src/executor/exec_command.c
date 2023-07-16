d84282784cbd02d18500482c33183159993e55f4
astahjmo <contato.johnatan06@gmail.com>
2023-07-16 15:17:24 -0300

    feat: mult commands

--- src/executor/exec_command.c
+++ src/executor/exec_command.c
@@ -6,7 +6,7 @@
 /*   By: johmatos <johmatos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 17:41:22 by johmatos          #+#    #+#             */
-/*   Updated: 2023/07/14 15:40:49 by johmatos         ###   ########.fr       */
+/*   Updated: 2023/07/16 14:58:09 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ char	**create_envs(void)
 	return (envps);
 }
 
-void	exec_command(t_node *cmd, t_node **free_if_invalid)
+void	exec_command(t_node *cmd)
 {
 	char	*path;
 	char	**args;
@@ -82,7 +82,7 @@ void	exec_command(t_node *cmd, t_node **free_if_invalid)
 	if (!path)
 	{
 		printf("minishell: %s: comando não encontrado\n", cmd->str);
-		free(free_if_invalid);
+		free(getter_data()->cmds->arr_cmds);
 		free_cmds(&ar);
 		free_all(getter_data());
 		free(path);
