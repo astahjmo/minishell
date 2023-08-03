61fb14d630b4972ab9770309d85751fade05db1a
Vinni-Cedraz <Vinni.vcr@gmail.com>
2023-08-03 09:18:33 -0300

    trust no tester
    new file:   caralhada_de_testes_pra_rodar_na_mao_comparar_output_exit_code_e_valgrind_antes_de_submeter/mand/10_parsing_hell.sh
    new file:   caralhada_de_testes_pra_rodar_na_mao_comparar_output_exit_code_e_valgrind_antes_de_submeter/mand/0_compare_parsing.sh
    new file:   caralhada_de_testes_pra_rodar_na_mao_comparar_output_exit_code_e_valgrind_antes_de_submeter/mand/1_builtins.sh
    new file:   caralhada_de_testes_pra_rodar_na_mao_comparar_output_exit_code_e_valgrind_antes_de_submeter/mand/1_pipelines.sh
    new file:   caralhada_de_testes_pra_rodar_na_mao_comparar_output_exit_code_e_valgrind_antes_de_submeter/mand/1_redirs.sh
    new file:   caralhada_de_testes_pra_rodar_na_mao_comparar_output_exit_code_e_valgrind_antes_de_submeter/mand/1_scmds.sh
    new file:   caralhada_de_testes_pra_rodar_na_mao_comparar_output_exit_code_e_valgrind_antes_de_submeter/mand/1_variables.sh
    new file:   caralhada_de_testes_pra_rodar_na_mao_comparar_output_exit_code_e_valgrind_antes_de_submeter/mand/2_correction.sh
    new file:   caralhada_de_testes_pra_rodar_na_mao_comparar_output_exit_code_e_valgrind_antes_de_submeter/mand/2_path_check.sh
    new file:   caralhada_de_testes_pra_rodar_na_mao_comparar_output_exit_code_e_valgrind_antes_de_submeter/mand/8_syntax_errors.sh
    new file:   caralhada_de_testes_pra_rodar_na_mao_comparar_output_exit_code_e_valgrind_antes_de_submeter/mand/9_go_wild.sh
    new file:   caralhada_de_testes_pra_rodar_na_mao_comparar_output_exit_code_e_valgrind_antes_de_submeter/mini_death/1_segfault.sh
    new file:   caralhada_de_testes_pra_rodar_na_mao_comparar_output_exit_code_e_valgrind_antes_de_submeter/no_env/10_no_env.sh
    new file:   caralhada_de_testes_pra_rodar_na_mao_comparar_output_exit_code_e_valgrind_antes_de_submeter/test.sh
    deleted:    roteiro_de_testes.txt

---
+++ caralhada_de_testes_pra_rodar_na_mao_comparar_output_exit_code_e_valgrind_antes_de_submeter/mini_death/1_segfault.sh
@@ -0,0 +1,108 @@
+echo |< echo segf
+
+echo segf >| echo is this invalid
+
+echo >          >           < "echo"
+
+echo >          >         | echo kekw
+
+echo >          >         | echo super valid
+
+echo <          <         > echo
+
+echo <  < <         > ok
+
+echo <  < | echo ok
+
+echo <  < |    < ok
+
+echo <      < |    > echo
+
+echo >>| echo super valid
+
+echo >>< "echo"
+
+echo < < < echo seegf
+
+echo > > < "echo"
+
+echo > > | echo kekw
+
+echo > > | echo super valid
+
+echo < < > echo
+
+echo < < < > ok
+
+echo >>| echo kekw
+
+echo | < echo segf
+
+echo <<| echo ok
+
+echo <<|< ok
+
+echo <<|> echo
+
+echo <<> echo
+
+echo <<<> ok
+
+echo segfault <"<<<"<<amazing
+.
+amazing
+
+echo seg < > echo seg
+
+echo seg > < echo segf
+
+echo seg < < > echo segf
+
+echo seg < < < > echo segf
+
+echo segf > | echo is this invalid
+
+echo segf < | < echo super valid
+
+echo < < | echo ok
+
+echo < < | < ok
+
+echo < < | > echo
+
+echo segfault < " < < < " < < amazing
+.
+amazing
+
+echo seg <  > echo seg
+
+echo seg >  < echo segf
+
+echo seg <      < > echo segf
+
+echo seg <      < <    > echo segf
+
+echo <      <     < echo seegf
+
+echo |      < echo segf
+
+echo segf >     | echo is this invalid
+
+echo segf <         |        < echo super valid
+
+
+echo segfault <"    <   <   <"  <   <   amazing
+.
+amazing
+
+echo seg <> echo seg
+
+echo seg >< echo segf
+
+echo seg <<> echo segf
+
+echo seg <<<> echo segf
+
+echo <<< echo seegf
+
+echo segf <|< echo super valid
